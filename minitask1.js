2453b9207768baf636a514ba97f4c9e7192a635f
ranandasatria <rananda24@gmail.com>
2025-04-17 11:43:24 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,12 @@
+function ganjilGenap(bilangan){
+    if (bilangan % 2 == 0){
+        return "Bilangan Genap";
+    }else{
+        return "Bilangan Ganjil";
+    }
+}
+
+console.log(ganjilGenap(2));
+console.log(ganjilGenap(5));
+console.log(ganjilGenap(248));
+console.log(ganjilGenap(793));
