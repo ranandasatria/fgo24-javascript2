f4a76a07b41d078129eea55f2c6a552c45aea28a
ranandasatria <rananda24@gmail.com>
2025-04-18 16:07:49 +0700

    Add files via upload

--- _callback.js
+++ _callback.js
@@ -11,10 +11,69 @@ const divide = (y1, y2) => {
 }
 
 const result1 = calculate(5,5, multiply)
-// const result2 = calculate(5,2, divide)
-// const result3 = calculate(5,2,function(a1,s2){
-// return a1+a2
-// })
+const result2 = calculate(5,2, divide)
 
 
 console.log(result1)
+
+function itung(num1, num2, cb){
+    return cb(num1, num2)
+}
+
+function penjumlahan(no1, no2){
+    return no1 + no2
+}
+
+console.log(itung(1, 2, penjumlahan))
+
+//ALUR PROGRAM
+
+function tampilkan(text) {
+    console.log("Masuk ke fungsi tampilkan")
+    console.log(text)
+  }
+  
+  function prosesNama(nama, callback) {
+    console.log("Masuk ke fungsi prosesNama")
+    let hasil = "Halo, " + nama
+    console.log("Hasil sudah dibuat:", hasil)
+    callback(hasil)
+  }
+  
+  console.log("Program dimulai")
+  prosesNama("Agung", tampilkan)
+  console.log("Program selesai")
+
+const sapa = (nama, kabar) =>{
+    return kabar(nama)
+}
+const tampilkanSapa = (nama) =>{
+    console.log(`Halo, ${nama}, apa kabar?`)
+}
+sapa("Agung", tampilkanSapa)
+
+
+  
+function prosesAngka(angka, callback){
+    callback(angka)
+}
+function tampilkanHasil(angka){
+    if(angka > 10){
+    console.log(`${angka} adalah bilangan besar`)
+    }else{
+    console.log(`${angka} adalah bilangan kecil`)
+    }
+}
+
+prosesAngka(15, tampilkanHasil)
+
+const prosesKata = (kata, callback) => {
+    return callback(kata)
+}
+
+const panjangKata = (kata) => {
+    console.log(`Panjang kata ${kata} adalah ${kata.length}`)
+
+}
+
+prosesKata("kocakgaming", panjangKata)
